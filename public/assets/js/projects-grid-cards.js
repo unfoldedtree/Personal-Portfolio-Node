a6650b92f620bdca8d5a9bd99d649c06c08dc35a
Will McMahan <66383989+unfoldedtree@users.noreply.github.com>
2021-09-22 12:25:32 +0000

    changed linking icons.

--- public/assets/js/projects-grid-cards.js
+++ public/assets/js/projects-grid-cards.js
@@ -7,28 +7,32 @@ const projectsArray = [
         description: "Called OnlyPosts this is a social media app that lets users add friends, see posts, react and like posts on their walls. Users can even customize their information in a profile page. Sign on with Google.",
         tech: ["HTML", "CSS", "JavaScript", "NodeJS", "MongoDB", "PassportJS", "Google OAuth"],
         img: "project-screenshot-social.png",
-        url: "https://social-media-crud-app.willmcmahan.repl.co"
+        git_url: "",
+        web_url: "https://social-media-crud-app.willmcmahan.repl.co"
     },
     {
         title: "Notes CRUD App",
         description: "This web app lets users sign on with Google. Then add notes to a wall. Demonstrates full stack functionality.",
         tech: ["HTML", "CSS", "JavaScript", "NodeJS", "MongoDB", "PassportJS", "Google OAuth"],
         img: "project-screenshot-notes.png",
-        url: "https://notes-crud-app.willmcmahan.repl.co"
+        git_url: "https://github.com/unfoldedtree/notes-crud-app",
+        web_url: "https://notes-crud-app.willmcmahan.repl.co"
     },
     {
         title: "Transactions App",
         description: "This full stack web app allows you to create accounts and add transactions. Sign on with Google.",
         tech: ["HTML", "CSS", "JavaScript", "NodeJS", "MongoDB", "PassportJS", "Google OAuth"],
         img: "project-screenshot-expenses.png",
-        url: "https://expense-tracker-crud-app.willmcmahan.repl.co"
+        git_url: "https://github.com/unfoldedtree/expense-tracker-crud-app",
+        web_url: "https://expense-tracker-crud-app.willmcmahan.repl.co"
     },
     {
         title: "Movies App",
         description: "Leverages a third party existing API to get a list of movies and display it to the user.",
         tech: ["HTML", "CSS", "JavaScript"],
         img: "project-screenshot-movies.png",
-        url: "www.test-movies.com"
+        git_url: "https://github.com/unfoldedtree/freetime-html-css-js/tree/main/movie-app",
+        web_url: ""
     }
 ]
 
@@ -44,15 +48,19 @@ function addProjectsToDOM () {
             <div class="project-info">
                 <h3>${project.title}</h3>
             </div>
-            <a href="${project.url}" target="_blank" style="text-decoration: none;">
             <div class="overview">
-                <h3>Overview</h3>
+                <div class="overview-head">
+                    <h3>Overview</h3>
+                    <div>
+                        ${project.web_url ? `<a href="${project.web_url}" target="_blank" style="text-decoration: none;"><i class="fas fa-globe btn-project-link"></i></a>` : ''}
+                        ${project.git_url ? `<a href="${project.git_url}" target="_blank" style="text-decoration: none;"><i class="fab fa-github btn-project-link"></i></a>` : ''}
+                    </div>
+                </div>
                 <p>${project.description}</p>
                 <div class="project-tech">
                     ${addTechToProject(project.tech)}
                 </div>
             </div>
-            </a>
             `
         projectsEl.appendChild(projectEl)
         count++;
