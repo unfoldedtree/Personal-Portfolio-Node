a5351484a22052a20a4741b059b07db263bf4711
Will McMahan <66383989+unfoldedtree@users.noreply.github.com>
2022-02-24 22:05:37 +0000

    fixed dynamic scroll effect on github api results

--- public/assets/js/github.js
+++ public/assets/js/github.js
@@ -1,34 +1,39 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = 'https://api.github.com/users/unfoldedtree'
 
 const main = document.getElementById('main')
 
-async function getUser() {
-    const username = 'unfoldedtree'
+let statsRendered = false;
+
+async function getUserInfoFromGitHub() {
     
     try {
-        const { data } = await axios(APIURL + username)
-        getRepos(username, data)
-    } catch(err) {
-        // if(err.response.status == 404) {
-        //     createErrorCard('No profile with this username')
-        // }
-    }
-}
-
-async function getRepos(username, profile) {
-    const createdDate = Date.parse(profile.created_at)
-    const now = Date.now()
-    const days = Math.floor(Math.abs(createdDate - now) / (36e5 * 24));
-    
-    try {
-        const { data } = await axios(APIURL + username + '/repos?sort=pushed')
-        totalProjects = data.length;
-
+        let totalProjects = 0;
         let sumCommits = 0;
+        let daysAsUser = 0;
+        let followers = 0;
 
-        if (sessionStorage.sumCommits) {
-            sumCommits = sessionStorage.sumCommits;
+        if (sessionStorage.daysAsUser && sessionStorage.followers) {
+            daysAsUser = sessionStorage.daysAsUser
+            followers = sessionStorage.followers
         } else {
+            const { data } = await axios(APIURL)
+            const createdDate = Date.parse(data.created_at)
+            const now = Date.now()
+            daysAsUser = Math.floor(Math.abs(createdDate - now) / (36e5 * 24));
+
+            followers = data.followers
+
+            sessionStorage.daysAsUser = daysAsUser
+            sessionStorage.followers = followers
+        }
+
+        if (sessionStorage.totalProjects && sessionStorage.sumCommits) {
+            totalProjects = sessionStorage.totalProjects
+            sumCommits = sessionStorage.sumCommits
+        } else {
+            const { data } = await axios(APIURL + '/repos?sort=pushed')
+            totalProjects = data.length;
+            
             for (const repo of data) {
                 const REPO_URL = `https://api.github.com/repos/unfoldedtree/${repo.name}/commits`
                 const { data } = await axios(REPO_URL)
@@ -36,22 +41,51 @@ async function getRepos(username, profile) {
                 sumCommits += data.length;
             }
 
+            sessionStorage.totalProjects = totalProjects
             sessionStorage.sumCommits = sumCommits
         }
-
-        document.getElementById("days-counter").setAttribute('data-purecounter-end', days);
-        document.getElementById("projects-counter").setAttribute('data-purecounter-end', totalProjects);
-        document.getElementById("commits-counter").setAttribute('data-purecounter-end', sumCommits);
-        document.getElementById("followers-counter").setAttribute('data-purecounter-end', days);
-        document.getElementById("followers-counter").setAttribute('data-purecounter-end', profile.followers);
-
-        // document.getElementById("days-counter").innerHTML.reload
         
+        document.getElementById("days-counter").setAttribute('data-count', daysAsUser);
+        document.getElementById("projects-counter").setAttribute('data-count', totalProjects);
+        document.getElementById("commits-counter").setAttribute('data-count', sumCommits);
+        document.getElementById("followers-counter").setAttribute('data-count', followers);
+
+        $(window).scroll(function() {
+            evaluateScroll()
+        });
+        
+        evaluateScroll()
     } catch(err) {
-        // createErrorCard('Problem fetching repos')
+        console.log("Error fetching from Github API.")
     }
 }
 
+function evaluateScroll() {
+    const oTop = $('#facts').offset().top - window.innerHeight;
+    if (!statsRendered && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+            const $this = $(this),
+            countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+            {
+                duration: 2000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        statsRendered = true;
+    }   
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-    getUser()
+    getUserInfoFromGitHub()
 });
